dc21c84609d59dbebf2b8eb19e4b9b69c6835eec
praatikgote <pratikgote69@gmail.com>
2023-02-28 11:25:58 +0530

    change

---
+++ backend/scripts/after_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#_Change_Working_Directory
+cd /home/ec2-user/demoworkspece/server2
+
+#_Remove_Unused_Code
+rm -rf node_modules
+#_rm -rf build ------
+
+#Install_node_modules_&_Make_React_Build
+npm  install
+#_npm run build ------
+ 
+ 
