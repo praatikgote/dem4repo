64f4d35da2cf1f511b1467d931b537712db3b7ab
pratikgote <74818044+praatikgote@users.noreply.github.com>
2023-03-03 00:07:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const index = require("./routes/index")
 
 app.use("/4DApp", index)
 
-const port  = process.env.PORT || 4004;
+const port  = process.env.PORT || 4000;
 
 app.listen(port , function(){
     console.log("Server Started");
